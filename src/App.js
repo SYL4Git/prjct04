4e865b43e16c6d0f4dda49bea55f86e5bf5d1261
SYL4Git <seungyeollee4@gmail.com>
2024-03-04 17:06:38 +0900

    240304 1706 @class

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 
+const dummy = [
+	{
+		id: 2,
+		content: '빨래',
+		createdDate: new Date().getTime(),
+		isDone: false,
+	},
+	{
+		id: 1,
+		content: '청소',
+		createdDate: new Date().getTime(),
+		isDone: false,
+	},
+	{
+		id: 0,
+		content: '설거지',
+		createdDate: new Date().getTime(),
+		isDone: false,
+	},
+];
+
 function App() {
-    return (
-        <div className="App">
-            
-        </div>
-    );
+	return (
+		<div className="App">
+			<BrowserRouter>
+				<Routes>
+					<Route></Route>
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
